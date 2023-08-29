dcef6b54a34e29b4316bb5093b90d24ca6c6210d
Ben Leadbetter <leadbetter.ben@outlook.com>
2023-08-29 16:54:23 +0100

    Save all songs from a chosen playlist

--- like-all-songs-in-playlist.py
+++ like-all-songs-in-playlist.py
@@ -2,7 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import dotenv_values
 
-scope = 'playlist-read-private'
+scope = 'playlist-read-private,user-library-modify'
 
 env = ".env.local"
 config = dotenv_values(env)
@@ -24,14 +24,17 @@ playlist_num = int(input("Which number playlist would you like to save the songs
 
 playlist_id = results['items'][playlist_num]['id']
 
-def show_tracks(results):
-    for item in results['items']:
-        track = item['track']
-        print("%32.32s %s" % (track['artists'][0]['name'], track['name']))
+def add_tracks(results):
+    """ Save all tracks from a list of results """
+    # Get list of track IDs from result of tracks
+    track_ids = [item['track']['id'] for item in results['items']]
+    sp.current_user_saved_tracks_add(track_ids)
 
-results = sp.playlist_items(playlist_id)
-show_tracks(results)
+# Save tracks add has a limit of 50 so need to limit this to 50
+# See https://developer.spotify.com/documentation/web-api/reference/save-tracks-user for details
+results = sp.playlist_items(playlist_id, limit=50)
+add_tracks(results)
 
 while (results['next']):
     results = sp.next(results)
-    show_tracks(results)
+    add_tracks(results)
